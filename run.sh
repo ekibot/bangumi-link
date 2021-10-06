ede2c20fdd1998fdc59002dbdd37af43770bc481
ekibot <soekibun+bot@gmail.com>
2021-10-06 11:47:37 +0800

    add bash script

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ cd data
 git config --local user.email "soekibun+bot@gmail.com"
 git config --local user.name "ekibot"
 tim=$(date "+%Y%m%d%H%M%S")
+cd data
 git add . -v
 git commit -m "Update at $time"
 git push
