bbd5436095f9dfd29aaeff0002b8018fbf717d98
Ubuntu <ekibun@ekibun.yqaqt0esqboexcq2cnajpu1qje.lx.internal.cloudapp.net>
2021-10-09 21:21:25 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ node spider.js
 cd data
 git config --local user.email "soekibun+bot@gmail.com"
 git config --local user.name "ekibot"
-tim=$(date "+%Y%m%d%H%M%S")
+time=$(date "+%Y%m%d%H%M%S")
 cd data
 git add . -v
 git commit -m "Update at $time"
