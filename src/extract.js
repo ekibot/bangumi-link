8d9190490381616d8610d906363a883b8c0c3e77
ekibun <soekibun@gmail.com>
2023-07-19 17:38:28 +0800

    fix bangumi dump

--- src/extract.js
+++ src/extract.js
@@ -1,3 +1,16 @@
-require('./relate');
-require('./check');
-require('./calendar');
+(async () => {
+  const archvieURL = 'https://api.github.com/repos/bangumi/Archive/releases/latest';
+  const data = await (await fetch(archvieURL)).json();
+  const dumpURL = data.assets.pop().browser_download_url;
+  const childProcess = require('child_process');
+  childProcess.execSync(`wget ${dumpURL} -O dump.zip`, {
+    stdio: 'inherit',
+  });
+  childProcess.execSync('unzip dump.zip -d ./archive', {
+    stdio: 'inherit',
+  });
+
+  require('./relate');
+  require('./check');
+  require('./calendar');
+})();
